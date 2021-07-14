452eed3ee7155f1a202b93841fdd98f5982b8259
Priscila <priferreira.psf@gmail.com>
2021-07-14 08:38:07 -0300

    atualizações

--- clube-de-viagens-stella/App.js
+++ clube-de-viagens-stella/App.js
@@ -5,7 +5,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 /* Páginas para rotas */
 import HomeScreen from './src/components/common/home';
-import LoginScreen from './src/components/common/login';
+import LoginScreen from './src/components/BackgroundPink/login';
+import AboutScreen from './src/components/BackgroundPink/about';
+import AccessScreen from './src/components/BackgroundPink/access';
+import AddressNotFoundScreen from './src/components/BackgroundPink/addressNotFound';
+import CompleteAddressScreen from './src/components/BackgroundPink/completeAddress';
+import InsertCodeScreen from './src/components/BackgroundPink/insertCode';
+
+import DreamTripScreen from './src/components/BackgroundWhite/dreamTrip';
+import HowItWorksScreen from './src/components/BackgroundWhite/howItWorks';
+import MemberPacksScreen from './src/components/BackgroundWhite/memberPacks';
 
 function CreatePostScreen({ navigation, route }) {
   const [postText, setPostText] = React.useState('');
@@ -60,11 +69,22 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="About">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+
+
         <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="About" component={AboutScreen}/>
+        <Stack.Screen name="Access" component={AccessScreen}/>
+        <Stack.Screen name="AddressNotFound" component={AddressNotFoundScreen}/>
+        <Stack.Screen name="CompleteAddress" component={CompleteAddressScreen}/>
+        <Stack.Screen name="InsertCode" component={InsertCodeScreen}/>
+
+        <Stack.Screen name="DreamTrip" component={DreamTripScreen}/>
+        <Stack.Screen name="HowItWorks" component={HowItWorksScreen}/>
+        <Stack.Screen name="MemberPacks" component={MemberPacksScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
