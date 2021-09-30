38d6c51b8a5a4d6bc688381a122bfdc925aeb9bd
PriFerreira <priferreira.psf@gmail.com>
2021-09-30 08:10:57 -0300

    Adicionadas alterações necessárias para melhoria do sistema.

--- clube-de-viagens-stella/App.js
+++ clube-de-viagens-stella/App.js
@@ -1,89 +1,35 @@
-import * as React from 'react';
-import { Button, View, Text, TextInput } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 
 /* Páginas para rotas */
-import AboutScreen from './src/components/BackgroundPink/about';
-import AccessScreen from './src/components/BackgroundPink/access';
-import AddressNotFoundScreen from './src/components/BackgroundPink/addressNotFound';
-import CompleteAddressScreen from './src/components/BackgroundPink/completeAddress';
-import DreamTripScreen from './src/components/BackgroundWhite/dreamTrip';
-import HomeScreen from './src/components/common/home';
-import HomeLoggedScreen from './src/components/pages/homeLogged/home-logged';
-import HowItWorksScreen from './src/components/BackgroundWhite/howItWorks';
-import InsertCodeScreen from './src/components/BackgroundPink/insertCode';
-import LocalizationScreen from './src/components/BackgroundPink/localization';
-import LoginScreen from './src/components/BackgroundPink/login';
-import MemberPacksScreen from './src/components/BackgroundWhite/memberPacks';
-import PlanScreen from './src/components/BackgroundPink/plan';
-import RegisterScreen from './src/components/BackgroundPink/register';
-import RecoverPasswordScreen from './src/components/BackgroundPink/recoverPassword';
-import TermsConditionsScreen from './src/components/BackgroundPink/termsConditions';
-import ProfileScreen from './src/components/pages/profile';
-import FinancialAreaScreen from './src/components/pages/financialArea/index';
+import AboutScreen from "./src/components/pages/about";/**PG PRONTA, FALTA TRANSIÇÃO e interação com API ---- nÃO ENCONTREI NO FLUXO NOVO*/
+import AccessScreen from "./src/components/BackgroundPink/access";
+import AddressNotFoundScreen from "./src/components/BackgroundPink/addressNotFound";
+import CompleteAddressScreen from "./src/components/BackgroundPink/completeAddress";
+import DreamTripScreen from "./src/components/pages/dreamTrip";/* */
+import HomeScreen from "./src/components/common/home";
+import HomeLoggedScreen from "./src/components/pages/homeLogged/home-logged";
+import ClubScreen from "./src/components/pages/club";/* */
+import InsertCodeScreen from "./src/components/BackgroundPink/insertCode";
+import LocalizationScreen from "./src/components/BackgroundPink/localization";
+import LoginScreen from "./src/components/BackgroundPink/login";
+import PacksScreen from "./src/components/pages/memberPacks";/* */
+import PlanScreen from "./src/components/BackgroundPink/plan";
+import RegisterScreen from "./src/components/BackgroundPink/register";
+import RecoverPasswordScreen from "./src/components/BackgroundPink/recoverPassword";
+import TermsConditionsScreen from "./src/components/pages/termsConditions";/**PG PRONTA, FALTA TRANSIÇÃO e interação com API */
+import ProfileScreen from "./src/components/pages/profile";
+import FinancialAreaScreen from "./src/components/pages/financialArea/index";
+import PageFaceScreen from "./src/components/pages/pageFace";/**PG PRONTA, FALTA TRANSIÇÃO e interação com API */
 
-/*teste de rotas */
-function CreatePostScreen({ navigation, route }) {
-  const [postText, setPostText] = React.useState('');
-
-  return (
-    <>
-      <TextInput
-        multiline
-        placeholder="What's on your mind?"
-        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
-        value={postText}
-        onChangeText={setPostText}
-      />
-      <Button
-        title="Done"
-        onPress={() => {
-          // Pass and merge params back to home screen
-          navigation.navigate({
-            name: 'Home',
-            params: { post: postText },
-            merge: true,
-          });
-        }}
-      />
-    </>
-  );
-}
-
-function DetailsScreen({ route, navigation }) {
-  const { itemId, otherParam } = route.params;
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() =>
-          navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-          })
-        }
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
+/*TODO: Estudar alteração das pgs dream, howitworks e memberpacks para slides*/
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-/*function Root() {
-  return (
-      <Stack.Navigator initialRouteName="Login">
-        
-      </Stack.Navigator>
-  );
-}*/
 
 /* TODO: desenvolvi as próximas telas sem passar pelo 
 fluxo de login para acelerar:
@@ -94,7 +40,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-      initialRouteName="FinancialArea"
+      initialRouteName="DreamTrip"
       screenOptions={{
         headerShown: false
       }}
@@ -107,11 +53,12 @@ export default function App() {
         <Stack.Screen name="FinancialArea" component={FinancialAreaScreen}/>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="HomeLogged" component={HomeLoggedScreen}/> 
-        <Stack.Screen name="HowItWorks" component={HowItWorksScreen}/>
+        <Stack.Screen name="Club" component={ClubScreen}/>
         <Stack.Screen name="InsertCode" component={InsertCodeScreen}/>
         <Stack.Screen name="Localization" component={LocalizationScreen}/>
         <Stack.Screen name="Login" component={LoginScreen}/>
-        <Stack.Screen name="MemberPacks" component={MemberPacksScreen}/>
+        <Stack.Screen name="Packs" component={PacksScreen}/>
+        <Stack.Screen name="PageFace" component={PageFaceScreen}/> 
         <Stack.Screen name="Plan" component={PlanScreen}/> 
         <Stack.Screen name="Profile" component={ProfileScreen}/> 
         <Stack.Screen name="RecoverPassword" component={RecoverPasswordScreen}/>
