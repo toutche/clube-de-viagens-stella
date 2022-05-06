7af8a4766627133097069d026159a49dba1ac8da
felipessouza <felipe.souza@toutche.com.br>
2022-05-06 19:19:56 -0300

    busca endereco por cep

--- src/pages/Sign/GetLocation/index.js
+++ src/pages/Sign/GetLocation/index.js
@@ -33,23 +33,29 @@ import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplet
 import { AntDesign } from "@expo/vector-icons";
 import { consts } from "../../../utils/consts";
 import CustomIcon from "../../../components/CustomIcon";
+import CustomButton from "../../../components/CustomButton";
 import { logout } from "../../../services/auth";
 const titlePage = "Você pode nos ajudar e indicar o seu endereço?.";
 const subtitlePage = "Sabendo um pouco mais de você, poderemos conectar você com a sua próxima viagem!";
 const text = "";
+import api from "../../../services/api";
+import MaskInput from "react-native-mask-input";
+const zipCodeMask = [/\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/];
 
 const GetLocation = ({ navigation }) => {
   const googleRef = useRef();
 
   const [panel, setPanel] = useState(0);
   const [isKeyboard, setIsKeyboard] = useState(false);
-
   const [address, setAddress] = useState("");
   const [addressArr, setAddressArr] = useState([]);
   const [number, setNumber] = useState("");
+  const [CEP, setCEP] = useState("");
+  const [autocomplete, setAutocomplete] = useState("");
+  const [isAutocompleteVisible, setIsAutocompleteVisible] = useState(false);
 
   useEffect(() => {
-    address && googleRef?.current?.setAddressText(address);
+    // address && googleRef?.current?.setAddressText(address);
   }, [panel]);
 
   useEffect(() => {
@@ -91,12 +97,39 @@ const GetLocation = ({ navigation }) => {
     return array
   }
 
-  const pressHander = (datas, details) => {
-    setPanel(1);
-    setAddress(details.formatted_address);
-    setAddressArr(formatted_address(details.address_components))
-    setNumber("");
-    setIsKeyboard(false);
+  // const pressHander = (datas, details) => {
+  const pressHander = async (cep) => {
+    try {
+      setCEP(cep);
+      if(cep.length >= 8 ) {
+        await api.get(`https://viacep.com.br/ws/${cep}/json`)
+        .then(response => {
+          if(response.data.erro) {
+            setAutocomplete("CEP não encontrado!");
+          } else {
+            setAutocomplete(
+              response.data.logradouro + " - " +
+              response.data.bairro + " " +
+              response.data.complemento + " - " +
+              response.data.localidade + ", " +
+              response.data.uf
+              );
+            setAddress(response.data);
+            console.log(address)
+            // console.log(address);
+          }
+          setIsAutocompleteVisible(true);
+        })
+        .catch(e => {
+          throw e.message;
+        })
+      } else {
+        setAutocomplete("");
+        setIsAutocompleteVisible(false);
+      }
+    } catch(e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -126,7 +159,7 @@ const GetLocation = ({ navigation }) => {
 
               <Text style={Style.subtitle}>{subtitlePage}</Text>
 
-              <Text style={Style.subtitle}>{text}</Text>
+              <Text style={Style.subtitle}>{}</Text>
             </>
           : null}
 
@@ -136,13 +169,38 @@ const GetLocation = ({ navigation }) => {
             address={address}
             addressArr={addressArr}
             numberAddress={number}
+            CEP={CEP}
             onChange={text => setAddress(text)}
             changePanel={index => setPanel(index)}
             onChangeKeyboard={onChange => setIsKeyboard(onChange)}
             navigation={navigation}
           />
           :
-          <GooglePlacesAutocomplete
+          <View>
+            <MaskInput
+              value={CEP}
+              // onChange={ () => pressHander() }
+              onChangeText={ (masked, unmasked) => {
+                pressHander(unmasked)
+              }} 
+              mask={zipCodeMask}
+              style={Style.input}
+              placeholder='Digite seu CEP'
+              autoComplete='postal-code'
+            />
+            {
+              isAutocompleteVisible ?
+                <TouchableOpacity 
+                  onPress={() => setPanel(1)} 
+                  value={autocomplete}
+                  style={Style.autocompleteContainer}
+                >
+                  <Text style={Style.autocomplete}> { autocomplete } </Text>
+                </TouchableOpacity> 
+              : <></>
+            }
+          </View>
+          /* <GooglePlacesAutocomplete
             ref={googleRef}
             placeholder='Digite o seu Endereço ou CEP'
             enablePoweredByContainer={false}
@@ -167,7 +225,7 @@ const GetLocation = ({ navigation }) => {
             GooglePlacesDetailsQuery={{ fields: "formatted_address,geometry,address_component" }}
             filterReverseGeocodingByTypes={["locality", "administrative_area_level_3"]}
             debounce={200}
-          />
+          /> */
         }
       </View>
 
@@ -218,6 +276,31 @@ const Style = StyleSheet.create({
     fontSize: 14,
     textAlign: "center",
     marginBottom: 10,
+  },
+  input: {
+    fontSize: 14.5,
+    marginHorizontal: 2,
+    borderWidth: 1,
+    borderColor: "#d1d1d1",
+    borderRadius: 100,
+    height: 40,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    fontFamily: FONT_DEFAULT_STYLE,
+    textAlign: "center"
+  },
+  autocompleteContainer: {
+    backgroundColor: "#FFF",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderColor: "#d1d1d1",
+    borderRadius: 10,
+    marginTop: 10,
+    elevation: 5,
+    textAlign: "left"
+  },
+  autocomplete: {
+    fontFamily: FONT_DEFAULT_STYLE,
   },
 });
 
