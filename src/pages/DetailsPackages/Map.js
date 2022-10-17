2aeb18ed1b5209e17228ef6f95a5c4ce7539a931
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-10-17 18:15:19 -0300

    atualizado construção de endereço no detalhes de pacote e implementado botao para mostrar ou esconder mapa

--- src/pages/DetailsPackages/Map.js
+++ src/pages/DetailsPackages/Map.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import CustomButton from "../../components/CustomButton";
 import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
@@ -7,24 +7,48 @@ import * as Clipboard from "expo-clipboard";
 import { BLUE_COLOR, FONT_DEFAULT_STYLE } from "../../utils/variables";
 import * as Location from 'expo-location';
 import { LocationEventEmitter } from "expo-location/build/LocationEventEmitter";
+import { FontAwesome5, Entypo } from '@expo/vector-icons'; 
+import { useFilter } from "../../contexts/filter";
+import { BackHandler } from "react-native";
+import { Alert } from "react-native";
 
-const Map = ({ address, region }) => {
+const Map = ({ address, region, name, navigation }) => {
   const [location, setLocation] = useState([]);
+  const [isEnable, setIsEnable] = useState(false);
+  const [status, setStatus] = useState('');
+  const { autoScroll, setAutoScroll } = useFilter();
 
   useEffect(() => {
     (async () => {
+      // let { status } = await Location.requestForegroundPermissionsAsync();
+      // setStatus(status)
+      // if (status !== 'granted') {
+      //   setErrorMsg('Permission to access location was denied');
+      //   return;
+      // }
       let address = await Location.reverseGeocodeAsync(region);
       setLocation(address.length && address[0]);
     })();
+  }, []);
+
+  useEffect(() => {
+    const backAction = () => {
+      setAutoScroll(false);
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
   }, []);
   
   const copyToClipboard = () => {
     Clipboard.setString(address);
   };
   
-  // console.log(location !== null && location[0]);
-  console.log(location);
-      return (
+  return (
     <View>
       <Text style={styles.text}>Localização</Text>
 
@@ -35,16 +59,37 @@ const Map = ({ address, region }) => {
           size={36}
           color='#287dfd'
         />
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, height: 50, justifyContent: 'center' }}>
           <Text style={styles.title}>Endereço</Text>
           <Text style={styles.subTitle}>
             {
+              status !== 'granted' ? name : 
               location && (
                 `${location.country && location.country}${location.region !== null && location.region !== 's/n' && location.region && ', '}${location.region !== null && location.region !== 's/n' && location.region ? location.region : ''}${location.country && ', '}${location.street && location.street}${location.streetNumber !== null && location.streetNumber && location.streetNumber !== 's/n' ? ', ' : ''}${location.streetNumber !== 's/n' && location.streetNumber !== null ? location.streetNumber : ''}${location.subregion && location.subregion !== null ? ', ' : ''}${location.subregion !== null ? location.subregion : ''}`
               )
             }
           </Text>
         </View>
+        <TouchableOpacity
+        onPress={() => {
+          setIsEnable(!isEnable)
+          setAutoScroll(!autoScroll);
+        }}
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: 50,
+          width: 50,
+        }}>
+          {
+            isEnable ?
+            // <FontAwesome5 name="smile" size={32} color="black" />
+            <Entypo name="arrow-with-circle-up" size={32} color="black" />
+            :
+            <Entypo name="arrow-with-circle-down" size={32} color="black" />
+            // <Entypo name="emoji-sad" size={32} color="black" />
+          }
+        </TouchableOpacity>
       </View>
 
       <CustomButton
@@ -56,16 +101,19 @@ const Map = ({ address, region }) => {
         title={"Copiar Endereço"}
         onPress={copyToClipboard}
         containerStyle={styles.button_address}
-      />
+        />
 
-      <MapView
+      {
+        isEnable &&
+        <MapView
         region={region}
         style={styles.map}>
-        <Marker coordinate={{
-          latitude: region.latitude,
-          longitude: region.longitude
-        }} />
-      </MapView>
+          <Marker coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude
+          }} />
+        </MapView>  
+      }
     </View>
   );
 };
