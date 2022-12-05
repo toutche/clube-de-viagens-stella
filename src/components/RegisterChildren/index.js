6cbf694ddd8d4e49fe1f74791339751b16054fc3
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-12-05 08:58:51 -0300

    implementado validações e alerta no cadastro da criança

--- src/components/RegisterChildren/index.js
+++ src/components/RegisterChildren/index.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Alert } from "react-native";
+import { maskDate } from "../../utils/masks";
 import { Button, ButtonTitle, Container, StyledInput, Title } from "./style";
 
 export function RegisterChildren({ title, children, setChildren }) {
@@ -11,10 +13,22 @@ export function RegisterChildren({ title, children, setChildren }) {
     passport: '',
   });
 
-  // console.log(obj);
-
   function handleChildren() {
-    setChildren([...children, obj])
+    console.log(children);
+    if (!children.some((element) => element.cpf === obj.cpf)) {
+      setChildren([...children, obj])
+    } else {
+      Alert.alert(
+        "Criança já cadastrada",
+        "Altere os dados para cadastrar corretamente.",
+        [
+          {
+            text: "Cancel",
+            style: "cancel"
+          },
+        ]
+      );
+    }
   }
 
   return (
@@ -24,26 +38,35 @@ export function RegisterChildren({ title, children, setChildren }) {
       <StyledInput
         placeholder="Insira o nome *"
         onChangeText={(text) => setObj({ ...obj, name: text })}
+        value={obj.name}
+        minlength={3}
       />
 
       <StyledInput
         placeholder="Insira o sobrenome *"
         onChangeText={(text) => setObj({ ...obj, last_name: text })}
+        value={obj.last_name}
+        minlength={3}
       />
 
       <StyledInput
         placeholder="Data de nascimento *"
-        onChangeText={(text) => setObj({ ...obj, birth_date: text })}
+        onChangeText={(text) => setObj({ ...obj, birth_date: maskDate(text) })}
+        value={obj.birth_date}
+        keyboardType={"numeric"}
       />
 
       <StyledInput
         placeholder="CPF *"
         onChangeText={(text) => setObj({ ...obj, cpf: text })}
+        value={obj.cpf}
+        maxLength={11}
       />
 
       <StyledInput
         placeholder="Passaporte (Viagem Internacional) *"
         onChangeText={(text) => setObj({ ...obj, passport: text })}
+        value={obj.passport}
       />
 
       <Button onPress={() => handleChildren()}>
