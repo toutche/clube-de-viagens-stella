5d505518f101ccd2341bf8b902bf2786ddc5cfd2
Paulo Victor <pvmg1@hotmail.com>
2022-12-26 09:34:10 -0300

    implementado navigate.push para navegação entre pacotes

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -32,7 +32,7 @@ const ListItem = ({ item, index, display, navigation, plan, refreshList }) => {
 
   const handlePressLeftButton = () => {
     if (display === 0) {
-      navigation.push('DetailsPackages', { id: item.id })
+      navigation.push("DetailsPackages", { id: item.id });
     } else if (display === 1) {
       navigation.navigate({
         name: "DetailsHotels",
