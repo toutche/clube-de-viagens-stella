9c59a5884594cc8e54e5b6579943a144974a60b8
Dev Felipe Nunes <felipe.oliveira@stellabarros.com.br>
2023-01-10 11:00:11 -0300

    modificado alerta covid

--- src/components/AlertCovid/index.js
+++ src/components/AlertCovid/index.js
@@ -2,16 +2,18 @@ import React from "react";
 import { View, StyleSheet, Text, Image } from "react-native";
 import { useAuth } from "../../contexts/auth";
 import { FONT_DEFAULT_STYLE, PRIMARY_COLOR } from "../../utils/variables";
+import { Feather } from "@expo/vector-icons";
 
 const AlertCovid = ({ containerStyle, text_alert }) => {
   const { user } = useAuth();
 
   return (
     <View style={[styles.container, containerStyle]}>
-      <Image
+      {/* <Image
         source={{ uri: user.images.covid }}
         style={{ width: 33, height: 33, marginRight: 8 }}
-      />
+      /> */}
+      <Feather name='alert-triangle' size={40} color='red' />
       <Text style={styles.text}>{text_alert}</Text>
     </View>
   );
@@ -19,20 +21,22 @@ const AlertCovid = ({ containerStyle, text_alert }) => {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "center",
-    backgroundColor: PRIMARY_COLOR,
-    paddingHorizontal: 10,
-    paddingVertical: 15,
+    backgroundColor: "#FCE3E3",
+    paddingHorizontal: 30,
+    paddingVertical: 30,
     borderRadius: 15,
     marginVertical: 15,
     marginHorizontal: 15,
   },
   text: {
     fontFamily: FONT_DEFAULT_STYLE,
-    color: "#e1e1e1",
     fontSize: 13,
     flex: 1,
+    marginTop: 10,
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
 
